8ef5b809bf065b8a05171ad759395d0cae61acc7
Camila Rojas S <56646322+Camirojasserantoni@users.noreply.github.com>
2020-07-04 03:29:22 -0400

    showing some data from the api by default

--- src/Componentes/Informacion.js
+++ src/Componentes/Informacion.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
-function Informacion() { 
-    return <p>  DESDE INFO    </p>
+function Informacion({info}) { 
+    return (
+        <div className="card border-light">
+            <div className="card-header bg-primary text-light font-weight-bold">
+            Información Artista            
+            </div>
+            <div className="card-body">
+                <img src={info.strArtistThumb} alt="Logo Artista"/>
+                <p className="card-text"> Género Musical: {info.strGenre} 
+                </p>
+                <h2 className="card-text text-center"> Biografía</h2>
+    <p className="card-text"> {info.strBiographyES}</p>
+                <p className="card-text "> 
+                <a href={`https://${info.strFacebook}`} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-facebook"></i>
+                </a>
+                <a href={`https://${info.strTwitter}`} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-twitter"></i>
+                </a>
+                <a href={`${info.strLastFMChart}`} target="_blank" rel="noopener noreferrer">
+                    <i className="fab fa-lastfm"></i>
+                </a>
+                </p>
+
+            </div>    
+
+
+
+        </div> 
+
+    )
 
 
 }
